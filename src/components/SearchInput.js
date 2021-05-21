5cff0854cafad25fc223c9f227c1355690a60c7e
rastkotodorovic <rale.todorovic@gmail.com>
2021-05-22 01:47:47 +0200

    react youtube

---
+++ src/components/SearchInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class SearchInput extends React.Component {
+    state = {searchValue: ''};
+
+    getSearchValue = e => {
+        e.preventDefault();
+
+        this.props.onSubmit(this.state.searchValue);
+    };
+
+    render() {
+        return (
+            <div className="container mt-4">
+                <form onSubmit={ this.getSearchValue }>
+                    <div className="input-group mb-3">
+                        <input type="text" onChange={ e => this.setState({ searchValue: e.target.value }) } className="form-control" placeholder="Search..." />
+                        <div className="input-group-append">
+                            <button className="btn btn-primary">Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchInput;
